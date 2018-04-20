d02569f4fa0aa882007fac697de721e7e83825eb
corbynb <38438539+corbynb@users.noreply.github.com>
2018-04-19 19:31:52 -0600

    Delete falsePosCall.m

--- Matlab homework/falsePosCall.m
+++
@@ -1,7 +0,0 @@
-m = 25;
-g = 9.81;
-u = 0.55;
-F = 150;
-syms func(theta)
-func(theta) = ((u*m*g)/(cos(theta)+u*sin(theta))-F)
-[root,fx,ea,iter] = falsePosition_peer2(@(theta) func(theta), 0.1, 1.5)
